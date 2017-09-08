4d5073edf993637be77decb26536944c1acd9493
Bunix <etechnohubshop@gmail.com>
2017-09-08 19:18:29 +0800

    messagebird upload

---
+++ dashboard.messagebird/app/en-au/forgot-username.php
@@ -0,0 +1,151 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="" lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Recover your username - MessageBird</title><meta name="description" content="MessageBird is the complete solution for companies to use text messages for marketing, notification and communication purposes. Integrate our services in your own software or use our website."><meta name="csrf-param" content="_csrf">
+    <meta name="csrf-token" content="dXlLV0xNT20iG3gnLnkaOjgJGRABBjcZGBMUJxQ1DhkjGy9gDRoBKg==">
+
+<!-- Favicon -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://dash.cdn.messagebird.com/assets/images/favicons/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="https://dash.cdn.messagebird.com/assets/images/favicons/apple-touch-icon-152x152.png">
+        <link rel="icon" type="image/png" href="https://dash.cdn.messagebird.com/assets/images/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="https://dash.cdn.messagebird.com/assets/images/favicons/favicon-16x16.png" sizes="16x16"><link rel="icon" type="image/x-icon" href="https://dash.cdn.messagebird.com/assets/images/favicons/favicon.ico"><meta name="application-name" content="&nbsp;">
+        <meta name="msapplication-TileColor" content="#FFFFFF">
+        <meta name="msapplication-TileImage" content="https://dash.cdn.messagebird.com/assets/images/favicons/mstile-144x144.png">
+<script type="text/javascript">
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-34056529-1', {'cookieDomain': '.messagebird.com'});
+ga('create', 'UA-34056529-4', {'cookieDomain': '.messagebird.com', 'name': 'secondTracker'});
+ga('set', 'User Type', 'Guest');
+ga('secondTracker.set', 'User Type', 'Guest');
+ga('require', 'displayfeatures');
+ga('secondTracker.require', 'displayfeatures');
+ga('send', 'pageview');
+ga('secondTracker.send', 'pageview');
+
+</script>
+<script type="text/javascript">
+            !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";
+            analytics.load("I2Py1njgrCIuI8Q7kTZba08d8knyzK1k");
+            }}();
+        </script><script>analytics.page("", {route: "app/login/forgot-username"});</script><script type="text/javascript">
+            var _user_id = "";
+            var _session_id = "qknofvmq2fe1hcne2oc50q7214";
+            var _sift = window._sift = window._sift || [];
+
+            _sift.push(["_setAccount", "ed76f300ec"]);
+            _sift.push(["_setUserId", _user_id]);
+            _sift.push(["_setSessionId", _session_id]);
+            _sift.push(["_trackPageview"]);
+
+            (function() {
+                function ls() {
+                    var e = document.createElement("script");
+                    var s = document.getElementsByTagName("script")[0];
+                    e.src = "https://cdn.siftscience.com/s.js";
+                    s.parentNode.insertBefore(e, s);
+                }
+                if (window.attachEvent) {
+                    window.attachEvent("onload", ls);
+                } else {
+                    window.addEventListener("load", ls, false);
+                }
+               })();
+        </script>
+<script>
+    var lang_processing = 'Processing';
+    var lang_saving = 'Saving';
+</script>
+<link href="https://dash.cdn.messagebird.com/assets/css/all-814764ee46e8ea6826d7a08221e5a3ddaa748ff2.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+    </head>
+        <body class="payment-flow login compact">
+        <div class="body-background schoolboy-q">
+
+            
+            
+            <div class="container">
+                <div class="row">
+                    <section class="col-md-12 page-content" style="margin-top: 60px;">
+                        <header class="payment-header text-center">
+                            <h1><img src="..\..\assets\images\logo\logo-white.svg" height="40" alt="logo"></h1>
+                        </header>
+                    </section>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row payment-box">
+
+                    <section class="page-content" style="padding-bottom: 15px;">
+                        <form id="forgot-username" action="/app/en-au/forgot-username" method="post">
+<input type="hidden" name="_csrf" value="dXlLV0xNT20iG3gnLnkaOjgJGRABBjcZGBMUJxQ1DhkjGy9gDRoBKg==">
+
+    <div class="login-box">
+
+        <header class="text-center">
+            <h1 class="signup-heading">Recover your username</h1>
+            <p class="signup-description">Fill in your username below and we will send you an email with further instructions.</p>
+        </header>
+        
+        	<div class="row">	<div class="col-md-12">
+
+		<div class="form-group field-forgotusernameform-emailaddress required">
+		<label class="control-label" for="forgotusernameform-emailaddress">Email address</label>
+		<input type="text" id="forgotusernameform-emailaddress" class="form-control" name="ForgotUsernameForm[emailaddress]" placeholder="Email address" aria-required="true">
+		
+		<span class="error-msg"></span>
+		</div>
+		
+	</div>
+</div>
+    </div>
+
+    <div class="col-md-12" style="margin-top: 30px;">
+        <button type="submit" class="btn-primary btn">Recover username</button>        <p class="text-center agreement"><a href="/app/en-au/">Return to login</a></p>
+    </div>
+
+</form>                    </section>
+
+                </div>
+
+                            </div>
+
+            <footer class="checkout-footer">
+    <div class="container">
+        <div class="row payment-footer">
+        	&copy; 2017 MessageBird                    	<ul class="footer-nav sign-up">
+		        <li>
+		            <a href="https://www.messagebird.com/en/terms" class="skip-pjax">Terms</a>
+		        </li>
+		        <li>
+		            <a href="https://www.messagebird.com/en/privacy" class="skip-pjax">Privacy</a>
+		        </li>
+		    </ul>
+                    </div>
+    </div>
+</footer>
+<!-- env: prod-dc1-web-102  -->
+            <script src="https://dash.cdn.messagebird.com/assets/js/all-814764ee46e8ea6826d7a08221e5a3ddaa748ff2.js"></script>
+<script src="https://dash.cdn.messagebird.com/assets/26163275/yii2-dynamic-form.min.js"></script>
+<script type="text/javascript">jQuery(document).ready(function () {
+jQuery('#forgot-username').yiiActiveForm([{"id":"forgotusernameform-emailaddress","name":"emailaddress","container":".field-forgotusernameform-emailaddress","input":"#forgotusernameform-emailaddress","error":".error-msg","enableAjaxValidation":true,"validateOnType":true,"validationDelay":300,"validate":function (attribute, value, messages, deferred, $form) {yii.validation.required(value, messages, {"message":"Oops! Invalid email address."});yii.validation.email(value, messages, {"pattern":/^[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/,"fullPattern":/^[^@]*<[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?>$/,"allowName":false,"message":"Oops! Invalid email address.","enableIDN":false,"skipOnEmpty":1});}},{"id":"forgotusernameform-emailaddress","name":"emailaddress","container":".field-forgotusernameform-emailaddress","input":"#forgotusernameform-emailaddress","error":".error-msg","enableAjaxValidation":true,"validateOnType":true,"validationDelay":300,"validate":function (attribute, value, messages, deferred, $form) {yii.validation.required(value, messages, {"message":"Oops! Invalid email address."});yii.validation.email(value, messages, {"pattern":/^[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/,"fullPattern":/^[^@]*<[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?>$/,"allowName":false,"message":"Oops! Invalid email address.","enableIDN":false,"skipOnEmpty":1});}}], {"errorCssClass":"error","successCssClass":"success"});
+
+});</script>
+        </div>
+    </body>
+    </html>
